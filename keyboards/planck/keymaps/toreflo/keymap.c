04d8601cab29ab068f67db6e7cec625febcd6273
toreflo <toreflo@NTU200241>
2020-05-07 19:30:17 +0200

    Added tap dance for ALT/L3

--- keyboards/planck/keymaps/toreflo/keymap.c
+++ keyboards/planck/keymaps/toreflo/keymap.c
@@ -32,10 +32,14 @@ enum planck_layers {
 enum my_keycodes {
   L1 = SAFE_RANGE,
   L2,
-  ALT_L3,
   R1,
   R2,
   BACKLIT
+};
+
+//Tap dance enums
+enum {
+  ALT_L3 = 0,
 };
 
 #define BTICK ALGR(IT_QUOT)  // On Windows you need to define a custom Italian layout
@@ -45,6 +49,25 @@ enum my_keycodes {
 #define L2 MO(_SYMBOLS)
 #define R1 MO(_MEDIA_AND_NAV)
 #define R2 MO(_ALT_GR)
+
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
+
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3,
+  DOUBLE_HOLD = 4,
+  TRIPLE_TAP = 5,
+  TRIPLE_HOLD = 6
+};
+
+int cur_dance (qk_tap_dance_state_t *state);
+void alt_finished (qk_tap_dance_state_t *state, void *user_data);
+void alt_reset (qk_tap_dance_state_t *state, void *user_data);
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -60,10 +83,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,  IT_Q,    IT_W,    IT_E,    IT_R,    IT_T,    IT_Y,    IT_U,    IT_I,    IT_O,      IT_P,     IT_PLUS,
-    KC_ESC,  IT_A,    IT_S,    IT_D,    IT_F,    IT_G,    IT_H,    IT_J,    IT_K,    IT_L,      _______,  KC_ENT,
-    KC_LSFT, IT_Z,    IT_X,    IT_C,    IT_V,    IT_B,    IT_N,    IT_M,    IT_COMM, IT_DOT,    IT_MINS,  _______,
-    KC_LCTL, KC_LGUI, ALT_L3,  L2,      L1,      KC_SPC,  KC_SPC,  R1,      R2,      KC_INSERT, KC_DEL,   KC_BSPC
+    KC_TAB,  IT_Q,    IT_W,        IT_E,    IT_R,    IT_T,    IT_Y,    IT_U,    IT_I,    IT_O,      IT_P,     IT_PLUS,
+    KC_ESC,  IT_A,    IT_S,        IT_D,    IT_F,    IT_G,    IT_H,    IT_J,    IT_K,    IT_L,      _______,  KC_ENT,
+    KC_LSFT, IT_Z,    IT_X,        IT_C,    IT_V,    IT_B,    IT_N,    IT_M,    IT_COMM, IT_DOT,    IT_MINS,  _______,
+    KC_LCTL, KC_LGUI, TD(ALT_L3),  L2,      L1,      KC_SPC,  KC_SPC,  R1,      R2,      KC_INSERT, KC_DEL,   KC_BSPC
 ),
 
 /* L1: Numbers
@@ -206,8 +229,63 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return state;
 }
 
+int cur_dance (qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (state->pressed) return SINGLE_HOLD;
+    else return SINGLE_TAP;
+  }
+  else if (state->count == 2) {
+    if (state->pressed) return DOUBLE_HOLD;
+    else return DOUBLE_TAP;
+  }
+  else if (state->count == 3) {
+    if (state->interrupted || !state->pressed)  return TRIPLE_TAP;
+    else return TRIPLE_HOLD;
+  }
+  else return 8;
+}
+
+static tap alttap_state = {
+  .is_press_action = true,
+  .state = 0
+};
+
+void alt_finished (qk_tap_dance_state_t *state, void *user_data) {
+  alttap_state.state = cur_dance(state);
+  switch (alttap_state.state) {
+    case SINGLE_TAP: set_oneshot_layer(_FUNCTIONS, ONESHOT_START); clear_oneshot_layer_state(ONESHOT_OTHER_KEY_PRESSED); break;
+    case SINGLE_HOLD: register_code(KC_LALT); break;
+    //case DOUBLE_TAP: set_oneshot_layer(1, ONESHOT_START); set_oneshot_layer(1, ONESHOT_PRESSED); break;
+    case DOUBLE_HOLD: register_code(KC_LALT); layer_on(_FUNCTIONS); break;
+    //Last case is for fast typing. Assuming your key is `f`:
+    //For example, when typing the word `buffer`, and you want to make sure that you send `ff` and not `Esc`.
+    //In order to type `ff` when typing fast, the next character will have to be hit within the `TAPPING_TERM`, which by default is 200ms.
+  }
+}
+
+void alt_reset (qk_tap_dance_state_t *state, void *user_data) {
+  switch (alttap_state.state) {
+    case SINGLE_TAP: break;
+    case SINGLE_HOLD: unregister_code(KC_LALT); break;
+    //case DOUBLE_TAP: break;
+    case DOUBLE_HOLD: layer_off(_FUNCTIONS); unregister_code(KC_LALT); break;
+  }
+  alttap_state.state = 0;
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [ALT_L3]     = ACTION_TAP_DANCE_FN_ADVANCED(NULL, alt_finished, alt_reset)
+};
+
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    case KC_TRNS:
+    case KC_NO:
+      /* Always cancel one-shot layer when another key gets pressed */
+      if (record->event.pressed && is_oneshot_layer_active())
+      clear_oneshot_layer_state(ONESHOT_OTHER_KEY_PRESSED);
+      return true;
     case BACKLIT:
       if (record->event.pressed) {
         register_code(KC_RSFT);
@@ -323,7 +401,7 @@ bool music_mask_user(uint16_t keycode) {
   switch (keycode) {
     case L1:
     case L2:
-    case ALT_L3:
+    //case ALT_L3:
     case R1:
     case R2:
       return false;
--- keyboards/planck/keymaps/toreflo/rules.mk
+++ keyboards/planck/keymaps/toreflo/rules.mk
@@ -1,1 +1,3 @@
 SRC += muse.c
+
+TAP_DANCE_ENABLE=yes
